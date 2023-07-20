7649f8a892f22a3677488a81ac8228fdc7e9f241
22p21n0056-chan <60224796+channo39mz@users.noreply.github.com>
2023-07-20 14:03:53 +0700

    Add files via upload

---
+++ Lab3q2_65197.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a = 1;
+    int sum = 0;
+    int count = 0;
+    while (1)
+    {
+        scanf("%d" , &a);
+        if (a <= 0){
+            break;
+        }
+        sum += a;
+        count += 1;
+    }
+    float average = sum / count;
+    printf("%d \n" , sum);
+    printf("%d \n"  , count);
+    printf("%f" , average);
+}
